27fa38f3bffdd8d5ae12c47f5f944ecbce677f49
naughtykun <.>
2026-06-21 20:45:39 +0530

    1

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("Hello NK")
-print("update 👾")
+def deposit():
+  while True:
+    amount = input("Enter your deposit")
+    if amount.isdigit():
+       amount = int(amount)
+       if amount > 10:
+        break
+       else: print("deposit must be atleast 10$")
+    else: print(f"invalid deposit {amount}")
+  return amount
+
+def main():
+   balance = deposit()
+   
+
+
+       

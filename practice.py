d2f2565ae0275a02c5865b0dee065ed0e5befd4c
naughtykun <.>
2026-06-25 18:48:25 +0530

    1

--- practice.py
+++ practice.py
@@ -1,1 +1,21 @@
-print("git")
+import math
+principle = 0
+rate = 0
+time = 0
+
+while principle <= 0:
+      principle = float(input("Enter your principal amount:  $"))
+      print("invalid principle ")
+while rate <= 0:
+      rate = float(input("Enter your principal amount:  $"))
+      print("invalid rate ")
+while time <= 0:
+      time = int(input("Enter your principal amount:  $"))
+      print("invalid time ")
+      
+total = principle * pow((1 + rate / 100), time)
+      
+print(f"Balance after {time} years with original principle ${principle} and rate {rate} is {total}%")
+
+
+     

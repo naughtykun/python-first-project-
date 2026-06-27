556470fd9871ef3a0f2834959a6a39b0758fc2fd
naughtykun <.>
2026-06-27 16:13:47 +0530

    1

--- practice.py
+++ practice.py
@@ -1,13 +1,16 @@
 import time
 myTime = int(input("Whats your timer count in seconds? :  "))
 
+print("Starting the timer!")
+
+time.sleep(2)
+
 for x in range(myTime, 0 , -1): 
     seconds = x % 60
     minutes = int(x/60) % 60
     hours = int(x/3600)
     
-    print("Starting the timer!")
-    time.sleep(2)
+    time.sleep(1)
     print(f"{hours:02}:{minutes:02}:{seconds:02}")
     
     

c58b609d6186b12c83d72c5fdce53fb74d41e55f
naughtykun <.>
2026-06-27 17:01:57 +0530

    multiple?

--- practice.py
+++ practice.py
@@ -1,11 +1,13 @@
 import time
-myTime = int(input("Whats your timer count in seconds? :  "))
+myTime = int(input("Which project? :  "))
 
-print("Starting the timer!")
+if myTime == 1: 
+  realTime = int(input("Whats your timer count in seconds? :  "))
+  print("Starting the timer!")
 
-time.sleep(2)
+  time.sleep(2)
 
-for x in range(myTime, 0 , -1): 
+  for x in range(realTime, 0 , -1): 
     seconds = x % 60
     minutes = int(x/60) % 60
     hours = int(x/3600)
@@ -13,4 +15,15 @@ for x in range(myTime, 0 , -1):
     time.sleep(1)
     print(f"{hours:02}:{minutes:02}:{seconds:02}")
     
-    
+else: 
+
+ if myTime == 2:
+  rows = int(input("How many rows?:  "))
+  columns = int(input("How many columns?:  "))
+  symbol = input("Pick a symbol/Emoji:  ")
+  
+  for x in range(rows):
+     for y in range(columns):
+         print(symbol)
+         
+  
